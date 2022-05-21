d58988ea07661a6fb92a2a8b03ebd1438ff97ce0
camiloclaurediaz <camilo@shoptropicals.com>
2022-05-21 04:17:45 -0400

    updating test config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
     testEnvironment: 'node',
     testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    transform: {
+        "^.+\\.(t|j)sx?$": "ts-jest",
+    }
 };
--- src/tests/Application/Dto/RouteDto.test.ts
+++ src/tests/Application/Dto/RouteDto.test.ts
@@ -3,17 +3,18 @@ import {RouteDto} from "../../../Application/Dto/RouteDto";
 
 describe('Creating a RouteDto', () => {
     test('should create RouteDto', () => {
+        const date = new Date("2020-12-12");
         let route = new RouteDto();
         route.id = "11";
         route.name = "SCZCBBA";
         route.distance = 2323;
-        route.time = new Date("2020-12-12");
+        route.time = date
         route.origin = "SCZ";
         route.destiny = "CBBA";
-        expect(route.id).toBe("SCZCBBA");
-        expect(route.name).toBe("11");
+        expect(route.id).toBe("11");
+        expect(route.name).toBe("SCZCBBA");
         expect(route.distance).toBe(2323);
-        expect(route.time).toBe(new Date("2020-12-12"));
+        expect(route.time).toBe(date);
         expect(route.origin).toBe("SCZ");
         expect(route.destiny).toBe("CBBA");
     });
