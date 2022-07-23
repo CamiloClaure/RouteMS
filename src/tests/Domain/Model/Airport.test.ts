354bc474c229f3ee6807b69356dff8e048130b2a
camiloclaurediaz <camilo@shoptropicals.com>
2022-07-22 20:57:04 -0400

    Fixing test

--- src/tests/Domain/Model/Airport.test.ts
+++ src/tests/Domain/Model/Airport.test.ts
@@ -4,7 +4,7 @@ import { Airport } from '../../../Domain/Model/Airport/Airport';
 import { createMock } from 'ts-auto-mock';
 import { FlightService } from '../../../Application/Services/FlightService';
 import { Route } from '../../../Domain/Model/Route/Route';
-const date = new Date('2020-12-12');
+const date = new Date('2020-12-25T00:00:00');
 const route = createMock<Route>();
 route.name.Name = 'CBBASCZ';
 describe('Creating a FlightCodeGenerated', () => {
@@ -12,6 +12,6 @@ describe('Creating a FlightCodeGenerated', () => {
 		const airport = new Airport('Viru Viru');
 		expect(airport).not.toBe(null);
 		const flightCode = airport.getFlightCode(date, route);
-		expect(flightCode).toBe('11-CBBASCZ');
+		expect(flightCode).toBe('25-CBBASCZ');
 	});
 });
