65edd86094c87b712cb18efbb6b7de953898461a
CamiloClaure <camilo.claure@gmail.com>
2022-09-17 08:57:46 -0400

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,27 @@ import { CRUDCommandFactory } from './Application/UseCases/Command/CRUDCommandHa
 import { RouteService } from './Application/Services/RouteService';
 import { FlightService } from './Application/Services/FlightService';
 import { RouteRepository } from './Infrastructure/ORM/Repository/RouteRepository';
-
+import * as fs from 'fs';
+import * as path from 'path';
 @Module({
-	imports: [TypeOrmModule.forRoot()],
+	imports: [TypeOrmModule.forRoot({
+		type: "postgres",
+		host: "db-postgresql-nyc1-96651-do-user-12454790-0.b.db.ondigitalocean.com",
+		port: 25060,
+		username: "doadmin",
+		password: "AVNS_CN9Jp0ogwNX5Vnmr36y",
+		database: "defaultdb",
+		entities: ["dist/Infrastructure/ORM/Entities/**/*.js"],
+		synchronize: true,
+		migrations: ["src/persistance/migrations/*.js"],
+		migrationsTableName: "custom_migration_table",
+		logging: false,
+		ssl: {
+			ca: fs
+				.readFileSync(path.join(__dirname, '../src/ca-certificate.crt'))
+				.toString()
+		}
+	})],
 	controllers: [FlightController],
 	providers: [RouteService, FlightService, RouteRepository, CreateFlightCommandFactory, FlightRepository],
 })
